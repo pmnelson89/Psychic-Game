ee3bffb6957f420fdc99386dec5d629133859059
Paul Nelson <pmnelso@gmail.com>
2019-02-25 15:37:50 -0500

    changed javascript, added styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var guessesLeft = 10;
 var guessesSoFar = [];
 
 
@@ -16,18 +16,34 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
     var computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    if (userGuess === computerChoices[i]){
-       
-        if (userGuess === computerChoice) {
-            wins++;
-        } else {
-            losses++;
-            guessesLeft--;
-        }
-
-        userChoiceText.textContent = "Your guesses so far: " + userGuess;
-        winsText.textContent = "Wins: " + wins;
-        lossesText.textContent = "Losses: " + losses;
-        guessesLeftText.textContent = "Guesses Left: " + guessesLeft;
+    
+    if (userGuess === computerChoice){
+        win = wins++;
+        resetGame();
+        } 
+    // else {
+    //     guessesLeft = guessesLeft--; //starts at 10 but will not change
+    //     }
+    else {
+        guessesLeft--; //starts at 9 but changes
     }
-};
+
+    if (guessesLeft < 1) {
+        losses++;
+        resetGame();
+    }
+
+
+        guessesSoFarText.textContent = userGuess;
+        winsText.textContent = wins;
+        lossesText.textContent = losses;
+        guessesLeftText.textContent = guessesLeft;
+    
+}
+
+//where do I put this??
+
+function resetGame() {
+    guessesLeft = 10;
+    guessesSoFar = [];
+}
